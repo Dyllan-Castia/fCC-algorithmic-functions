8b39a6399e8d56746b142833a96d5a79936c9b32
Dyllan Castia <castiadyllan@gmail.com>
2025-12-06 18:45:54 -0800

    Refactor pyramid function for better output control

--- testLogic.js
+++ testLogic.js
@@ -1,35 +1,42 @@
 function pyramid(pattern, rows, bool) {
-  
+  let lines = [];
+  const maxWidth = 1 + 2 * (rows - 1);
+
   if (bool === false) {
- 
-  for (let i = rows; i <= rows && rows > 0; i--) {
 
-  let space = "@".repeat(i / 2 + ((i - 1) / 2));
+    for (let i = 0; i < rows; i++) {
+      const count = 1 + 2 * i;
+      const spaces = (maxWidth - count) / 2;
+      const line = " ".repeat(spaces) + pattern.repeat(count);
 
-  console.log(space + space);
+      lines.push(line);
+    }
+  } else {
+    
+    for (let i = rows - 1; i >= 0; i--) {
+    const count = 1 + 2 * i;
+    const spaces = (maxWidth - count) / 2;
+    const line = " ".repeat(spaces) + pattern.repeat(count);
 
+      lines.push(line);
+
+    }
   }
+  return "\n" + lines.join("\n") + "\n";
 }
-}
-const draw = pyramid("dude", 5, false);
+
+const draw = pyramid("i", 5, false);
 console.log(draw);
-//@@@@@@@@
-//@@@@@@
-//@@@@
-//@@
+//    i
+//   iii
+//  iiiii
+// iiiiiii
+//iiiiiiiii
 
-
-function test(pattern, rows) {
-for (let i = 0; i <= rows; i++) {
-  let pat = pattern.repeat(i);
-  console.log(pat);
-}
-return "";
-}
-const tester = test("f", 5);
-console.log(tester);
-//f
-//ff
-//fff
-//ffff
-//fffff
+const draw = pyramid("i", 5, true);
+console.log(draw);
+//iiiiiiiii
+// iiiiiii
+//  iiiii
+//   iii
+//    i
