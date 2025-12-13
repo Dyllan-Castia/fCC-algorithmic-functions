11c106643f6a363e26cad7d363e249f73075e6ba
Dyllan Castia <castiadyllan@gmail.com>
2025-12-12 22:23:13 -0800

    Uncomment console.log for sumAll function

---
+++ sumAllNum.js
@@ -0,0 +1,13 @@
+function sumAll(arr) {
+  const min = Math.min(arr[0], arr[1]);
+  const max = Math.max(arr[0], arr[1]);
+  let total = 0;
+
+  for (let i = min; i <= max; i++) {
+    total += i;
+  }
+
+  return total;
+}
+console.log(sumAll([5, 10]));
+//45
